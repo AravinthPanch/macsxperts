e09de281017d7e1d6abf7152fc630892052a379f
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2016-02-01 17:00:45 +0530

    Added: logs folder

--- macs-panel/scripts/cron.sh
+++ macs-panel/scripts/cron.sh
@@ -1,1 +1,1 @@
-* * * * * /home/aravinth/macsxperts/macs-panel/scripts/ssh-persist.sh > ssh-persist.log 2>&1
+* * * * * /home/aravinth/macsxperts/macs-panel/scripts/ssh-persist.sh > /home/aravinth/macsxperts/logs/ssh-persist.log 2>&1
