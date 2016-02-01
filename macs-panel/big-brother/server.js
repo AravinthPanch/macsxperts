454d1ea0f83b870f356aafe8eb699e1f352ba360
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2016-02-01 18:59:22 +0530

    Updated: logger added

--- macs-panel/big-brother/server.js
+++ macs-panel/big-brother/server.js
@@ -11,24 +11,27 @@ var path = require('path');
 var basicAuth = require('basic-auth-connect');
 const spawn = require('child_process').spawn;
 
+var winston = require('winston');
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {'timestamp': true});
+
+
 app.use(basicAuth('guest', 'macs@gp15'));
-
 app.use('/cam', express.static(__dirname));
-
 app.get('/', function (req, res) {
 
 	const ls = spawn('mjpg_streamer', ['-i', "/usr/lib/input_uvc.so -d /dev/video0 -y -r 640x480 -f 10", '-o', "/usr/lib/output_http.so -p 8090"]);
 
 	ls.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`);
+		winston.info(`stdout: ${data}`);
 	});
 
 	ls.stderr.on('data', (data) => {
-		console.log(`stderr: ${data}`);
+		winston.info(`stderr: ${data}`);
 	});
 
 	ls.on('close', (code) => {
-		console.log(`mjpg process exited with code ${code}`);
+		winston.info(`mjpg process exited with code ${code}`);
 	});
 
 	setTimeout(function () {
@@ -38,5 +41,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-	console.log('Big Brother is listening on port 3000!');
+	winston.info('Big Brother is listening on port 3000!');
 });
