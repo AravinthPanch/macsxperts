bffb2f80085cda82d50dda08d402036341244721
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2016-02-17 18:04:56 +0530

    Added: Macs API OSC Relay and dashboard

---
+++ macs-api/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Author: Aravinth Panchadcharam
+ * Email: me@aravinth.info
+ * Date: 17/02/16.
+ * Contributors:
+ */
+
+var oscPort = new osc.WebSocketPort({
+	url: "ws://localhost:8081"
+});
+
+oscPort.on("open", function () {
+	console.log("OSC Serial <=> Browser Relay is started");
+});
+
+oscPort.on("message", function (oscMsg) {
+	console.log("OSC", oscMsg);
+	switch (oscMsg.address) {
+		case "/ph/":
+			$("#phValue").html(oscMsg.args[0]);
+			break;
+		case "/ec/":
+			$("#ecValue").html(oscMsg.args[0]);
+			break;
+		case "/temp/":
+			$("#tempValue").html(oscMsg.args[0]);
+			break;
+	}
+});
+
+oscPort.open();
--- macs-api/server.js
+++ macs-api/server.js
@@ -7,16 +7,57 @@
  * Contributors:
  */
 
-// Instantiate a new OSC Serial Port.
-var serialPort = new osc.SerialPort({
-	devicePath: "/dev/ttyAMA0",
-	bitrate: "9600"
+var osc = require("osc"),
+	http = require("http"),
+	ws = require("ws"),
+	express = require("express"),
+	app = express(),
+	webServer = app.listen(8081, function () {
+		console.log("OSC Serial <=> Browser Relay is started");
+	}),
+	webSocket = new ws.Server({
+		server: webServer
+	}),
+	oscSocket,
+	oscSerial = new osc.SerialPort({
+		devicePath: "/dev/ttyAMA0",
+		bitrate: "115200"
+	});
+
+
+webSocket.on("connection", function (browserSocket) {
+	// On Client connection
+	console.log("Client Connected");
+
+	// Attach Browser Websocket to OSC Websocket
+	oscSocket = new osc.WebSocketPort({
+		socket: browserSocket
+	});
+
+	// Relay Browser OSC Message to Serial
+	oscSocket.on("message", function (browserMsg) {
+		oscSerial.send(browserMsg);
+	});
+
+	// Relay Serial OSC Message to Browser
+	oscSerial.on("message", function (serialMsg) {
+		oscSocket.send(serialMsg);
+	});
+
+
+	oscSocket.send({
+		address: "/ph/",
+		args: 5.8
+	});
+
+	oscSocket.send({
+		address: "/ec/",
+		args: 1200
+	});
+
+	oscSocket.send({
+		address: "/temp/",
+		args: 25.5
+	});
 });
 
-
-serialPort.on("message", function (oscMsg) {
-	console.log("An OSC message was received!", oscMsg);
-});
-
-// Open the port.
-serialPort.open();
