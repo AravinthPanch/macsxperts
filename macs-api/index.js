9131413f8435e1ce97331eb8eff8efe7b0dfaa6d
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2016-02-20 13:02:00 +0530

    Fixed: OSC address

--- macs-api/index.js
+++ macs-api/index.js
@@ -18,15 +18,15 @@ oscPort.on("open", function () {
 oscPort.on("message", function (oscMsg) {
 	console.log("OSC", oscMsg);
 	switch (oscMsg.address) {
-		case "/ph/":
+		case "/sense/ph/":
 			var phValue = oscMsg.args[0];
 			$("#phValue").html(phValue.toFixed(2));
 			break;
-		case "/ec/":
+		case "/sense/ec/":
 			var ecValue = oscMsg.args[0];
 			$("#ecValue").html(Math.round(ecValue));
 			break;
-		case "/temp/":
+		case "/sense/temp/":
 			var tempValue = oscMsg.args[0];
 			$("#tempValue").html(tempValue.toFixed(2));
 			break;
